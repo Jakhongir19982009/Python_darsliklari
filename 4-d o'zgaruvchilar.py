ea25862d42bbc2cb78f0645c4a81c8afd304c6c5
Jakhongir Tuychiev <127219504+Jakhongir19982009@users.noreply.github.com>
2023-03-07 20:08:40 +0300

    4-dars yuklandi
    Bismillahir Rohmanir Rohiym.
    Python darslarini boshladik.

---
+++ 4-d o'zgaruvchilar.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 23:05:44 2023
+
+@author: tuych
+"""
+
+ism = 'Jakhongir'
+familiya = 'Tuychiev'
+yosh = 25
+
+
+familiya = "To'ychiyev"
+ism = 'Jahongir'
+yosh = 28
+
+print(ism)
+print(familiya)
+print(yosh)
+
+a = 6
+b = 7
+c = (a+b)**2
+
+print(c)
+
+year = 2023
+born = 1998
+age = year-born
+
+print(age)
+
